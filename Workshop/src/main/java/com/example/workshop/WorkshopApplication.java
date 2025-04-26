a97d86388964da3d0cc266237174791e04ec3b1d
Tobiasz Kownacki <tobiasz.kownacki@gmail.com>
2025-04-26 15:00:44 +0200

    Changed project from JavaFx to Java Spring Boot

---
+++ Workshop/src/main/java/com/example/workshop/WorkshopApplication.java
@@ -0,0 +1,28 @@
+package com.example.workshop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class WorkshopApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		Integer v = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+		if (v != null && v == 1) {
+			System.out.println(" Connection works (SELECT 1 = " + v + ")");
+		} else {
+			System.err.println("✖ Connection does not work (Result: " + v + ")");
+		}
+	}
+}
---
+++ Workshop/src/test/java/com/example/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Context loads successfully.");
+	}
+
+}
