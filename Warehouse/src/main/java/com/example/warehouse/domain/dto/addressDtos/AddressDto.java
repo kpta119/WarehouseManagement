a6c1db524c902d0e11cf855b8357fcc490040c6c
kpta119 <kacper.pt119@gmail.com>
2025-05-21 16:46:01 +0200

    Small changes in AddressDto

--- Warehouse/src/main/java/com/example/warehouse/domain/dto/addressDtos/AddressDto.java
+++ Warehouse/src/main/java/com/example/warehouse/domain/dto/addressDtos/AddressDto.java
@@ -1,6 +1,8 @@
 package com.example.warehouse.domain.dto.addressDtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,8 @@ public class AddressDto {
     @NotBlank(message = "Street cannot be empty")
     private String street;
 
+    @NotNull(message = "Street number cannot be null")
+    @Positive(message = "Street number must be a positive number")
     private Integer streetNumber;
     private String postalCode;
     private String city;
