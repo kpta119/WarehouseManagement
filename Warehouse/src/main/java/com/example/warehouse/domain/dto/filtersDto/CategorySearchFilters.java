985322e7f9247f4699a12fb1728df2f4c621f011
Tobiasz Kownacki <tobiasz.kownacki@gmail.com>
2025-05-30 22:31:46 +0200

    Modified Get api/categories endpoint Added name filter.

---
+++ Warehouse/src/main/java/com/example/warehouse/domain/dto/filtersDto/CategorySearchFilters.java
@@ -0,0 +1,12 @@
+package com.example.warehouse.domain.dto.filtersDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategorySearchFilters {
+    private String name;
+}
--- Warehouse/src/main/java/com/example/warehouse/repositories/CategoryRepository.java
+++ Warehouse/src/main/java/com/example/warehouse/repositories/CategoryRepository.java
@@ -1,10 +1,20 @@
 package com.example.warehouse.repositories;
 
 import com.example.warehouse.domain.Category;
+import com.example.warehouse.domain.dto.filtersDto.CategorySearchFilters;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer>, JpaRepository<Category, Integer> {
+
+    @Query("""
+        SELECT c FROM Category c
+        WHERE (:#{#filters.name} IS NULL OR c.name LIKE %:#{#filters.name}%)
+""")
+    Page<Category> findAll(CategorySearchFilters filters, Pageable pageable);
 }
