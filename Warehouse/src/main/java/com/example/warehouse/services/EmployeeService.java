bc8ec9ffd4ffb8549052f4c3f2f2565e67948fca
Tobiasz Kownacki <tobiasz.kownacki@gmail.com>
2025-05-31 11:31:21 +0200

    Modified Get api/employees endpoint: - fixed name filter - changed regionName to regionId

--- Warehouse/src/main/java/com/example/warehouse/services/EmployeeService.java
+++ Warehouse/src/main/java/com/example/warehouse/services/EmployeeService.java
@@ -4,13 +4,14 @@ import com.example.warehouse.domain.Employee;
 import com.example.warehouse.domain.dto.employeeDtos.EmployeeDto;
 import com.example.warehouse.domain.dto.employeeDtos.EmployeeSummaryDto;
 import com.example.warehouse.domain.dto.employeeDtos.EmployeeWithHistoryDto;
+import com.example.warehouse.domain.dto.filtersDto.EmployeeSearchFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
-    Page<EmployeeSummaryDto> getEmployeesWithTransactionCount(String partOfNameOrSurname, String regionName, Integer minTransactions, Integer maxTransactions, Integer warehouseId, Pageable pageable);
+    Page<EmployeeSummaryDto> getEmployeesWithTransactionCount(EmployeeSearchFilter filters, Pageable pageable);
 
     Employee createEmployee(EmployeeDto request);
 
